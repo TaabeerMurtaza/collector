d00294db9ff972cea190f9c94a0ffe1140c55508
Taabeer Murtaza <tabeermurtaza@gmail.com>
2024-07-25 05:38:35 +0500

    added reset link

--- routes/dev.php
+++ routes/dev.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
 
 Route::get('reset_everything', function () {
-    Artisan::call("migrate:fresh");
+    Artisan::call("migrate:fresh"); 
     Artisan::call("db:seed DatabaseSeeder");
 });
